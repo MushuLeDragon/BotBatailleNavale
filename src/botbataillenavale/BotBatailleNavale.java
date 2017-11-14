676e523bb8de26bd1839d5facfecd953d9b1233e
MushuLeDragon <dragon.mushu@hotmail.com>
2017-11-14 12:07:46 +0100

    Add new class Connection()

--- src/botbataillenavale/BotBatailleNavale.java
+++ src/botbataillenavale/BotBatailleNavale.java
@@ -5,6 +5,7 @@
  */
 package botbataillenavale;
 
+import static botbataillenavale.Connection.Connection;
 import java.awt.Desktop;
 import java.net.URI;
 
@@ -20,32 +21,7 @@ public class BotBatailleNavale {
     public static void main(String[] args) {
         // TODO code application logic here
 
+        Connection();
         
-        String URL = "http://fr.battleship-game.org/";
-
-        try {
-
-            Desktop d = Desktop.getDesktop();
-            d.browse(new URI(URL));
-            
-
-            /*
-            URL monURL = new URL("http://localhost/testwww/test.txt");
-
-            URLConnection connexion = monURL.openConnection();
-            InputStream flux = connexion.getInputStream();
-
-            int donneesALire = connexion.getContentLength();
-
-            for (; donneesALire != 0; donneesALire--) {
-                System.out.print((char) flux.read());
-            }
-
-            // Fermeture de la connexion
-            flux.close();
-             */
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
